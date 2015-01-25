c673b942d508d46d058a65420715ca4992aef99c
Jackeagle <jackeagle102@gmail.com>
2015-01-25 10:51:11 -0500

    cmhw: Add KeyDisabler
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Inherit from msm8226-common
 -include device/samsung/msm8226-common/BoardConfigCommon.mk
 
@@ -5,6 +19,9 @@ LOCAL_PATH := device/samsung/kmini3g
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+# CMHW
+BOARD_HARDWARE_CLASS += device/samsung/kmini3g/cmhw
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
