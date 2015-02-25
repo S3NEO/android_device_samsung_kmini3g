8a66e2a498e7aac82683036912af84a57c15e1ef
Rox- <r.rozic97@gmail.com>
2015-02-25 19:49:30 +0100

    kmini3g: Kang consumerir from samsung msm8974

--- device.mk
+++ device.mk
@@ -27,6 +27,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# IR
+PRODUCT_PACKAGES += \
+    consumerir.msm8226
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
