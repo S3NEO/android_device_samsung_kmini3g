6e62cc77f916881b5c40fb2256bdc20809f90185
Jackeagle <jackeagle102@gmail.com>
2016-07-20 23:44:00 +0530

    kmini3g: Switch to standalone libinit - Add TWRP Support
    Change-Id: I525b834d62efecfcf37e5acbc90b9400047b2ec0
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- init/init_kmini3g.cpp
+++ init/init_kmini3g.cpp
@@ -34,7 +34,7 @@
 #include "log.h"
 #include "util.h"
 
-#include "init_msm.h"
+#define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
 
 void gsm_properties()
 {
@@ -42,17 +42,13 @@ void gsm_properties()
     property_set("ro.telephony.default_network", "0");
 }
 
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
+void vendor_load_properties()
 {
     char platform[PROP_VALUE_MAX];
     char bootloader[PROP_VALUE_MAX];
     char device[PROP_VALUE_MAX];
     char devicename[PROP_VALUE_MAX];
     int rc;
-
-    UNUSED(msm_id);
-    UNUSED(msm_ver);
-    UNUSED(board_type);
 
     rc = property_get("ro.board.platform", platform);
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
