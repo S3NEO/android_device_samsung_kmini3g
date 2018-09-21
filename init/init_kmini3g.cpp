02b08f6004f7bc3b412f8fa60c48c831114a6aa8
Arne Coucheron <arco68@gmail.com>
2018-09-21 02:52:56 +0200

    kmini3g: libinit: s/ro.vendor.product/ro.product.vendor/g
    Prefixing with ro.product.vendor is the correct way.
    
    Change-Id: I38a5b7217d1ac66412219f93d895c6aa0a65abe3

--- init/init_kmini3g.cpp
+++ init/init_kmini3g.cpp
@@ -60,15 +60,15 @@ void init_target_properties()
         /* kmini3g */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/kmini3gxx/kmini3g:6.0.1/MMB29M/G800HXXU1CRJ1:user/release-keys");
         property_override("ro.build.description", "kmini3gxx-user 6.0.1 MMB29M G800HXXU1CRJ1 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-G800H");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "kmini3g");
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-G800H");
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "kmini3g");
         gsm_properties();
     } else if (bootloader.find("G800HQ") == 0) {
         /* kmini3g turkish version */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/kmini3gxx/kmini3g:4.4.2/KOT49H/G800HXXU1ANL1:user/release-keys");
         property_override("ro.build.description", "kmini3gxx-user 4.4.2 KOT49H G800HXXU1ANL1 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-G800HQ");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "kmini3g");
+        property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-G800HQ");
+        property_override_dual("ro.product.device", "ro.product.vendor.device", "kmini3g");
         gsm_properties();
     } else {
         gsm_properties();
