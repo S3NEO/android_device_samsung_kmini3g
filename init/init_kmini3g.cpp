c2abd1420f820fd7068f7ef70381ec126ebbaa0e
Łukasz Patron <priv.luk@gmail.com>
2019-11-03 11:10:42 +0200

    kmini3g: init: Override Everything
    Change-Id: Id5cb7979bd2d2324bf0613cf0fcb8a3812a46824

--- init/init_kmini3g.cpp
+++ init/init_kmini3g.cpp
@@ -1,7 +1,6 @@
 /*
    Copyright (c) 2016, The Linux Foundation. All rights reserved.
    Copyright (c) 2017-2020, The LineageOS Project. All rights reserved.
-
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -14,7 +13,6 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
-
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -35,8 +33,10 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <android-base/file.h>
 #include <android-base/logging.h>
 #include <android-base/properties.h>
+#include <android-base/strings.h>
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
@@ -45,26 +45,31 @@
 #include "vendor_init.h"
 
 using android::base::GetProperty;
+using android::base::ReadFileToString;
+using android::base::Trim;
 using android::init::property_set;
 
-void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
+// copied from build/tools/releasetools/ota_from_target_files.py
+// but with "." at the end and empty entry
+std::vector<std::string> ro_product_props_default_source_order = {
+    "",
+    "product.",
+    "product_services.",
+    "odm.",
+    "vendor.",
+    "system.",
+};
 
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
         __system_property_update(pi, value, strlen(value));
-    else
+    } else if (add) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
 }
-
-void property_override_dual(char const system_prop[],
-        char const vendor_prop[], char const value[])
-{
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
-}
-
 
 void gsm_properties()
 {
@@ -77,19 +82,29 @@ void vendor_load_properties()
 
     std::string bootloader = GetProperty("ro.bootloader", "");
 
+    const auto set_ro_product_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
     if (bootloader.find("G800H") == 0) {
         /* kmini3g */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/kmini3gxx/kmini3g:6.0.1/MMB29M/G800HXXU1CRJ1:user/release-keys");
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/kmini3gxx/kmini3g:6.0.1/MMB29M/G800HXXU1CRJ1:user/release-keys");
+            set_ro_product_prop(source, "model", "SM-G800H");
+            set_ro_product_prop(source, "device", "kmini3g");
+        }
         property_override("ro.build.description", "kmini3gxx-user 6.0.1 MMB29M G800HXXU1CRJ1 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-G800H");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "kmini3g");
         gsm_properties();
     } else if (bootloader.find("G800HQ") == 0) {
-        /* kmini3g turkish version */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/kmini3gxx/kmini3g:4.4.2/KOT49H/G800HXXU1ANL1:user/release-keys");
+        /* s3ve3gjv */
+        for (const auto &source : ro_product_props_default_source_order) {
+            set_ro_product_prop(source, "fingerprint", "samsung/kmini3gxx/kmini3g:4.4.2/KOT49H/G800HXXU1ANL1:user/release-keys");
+            set_ro_product_prop(source, "model", "SM-G800HQ");
+            set_ro_product_prop(source, "device", "kmini3g");
+        }
         property_override("ro.build.description", "kmini3gxx-user 4.4.2 KOT49H G800HXXU1ANL1 release-keys");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-G800HQ");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "kmini3g");
         gsm_properties();
     } else {
         gsm_properties();
